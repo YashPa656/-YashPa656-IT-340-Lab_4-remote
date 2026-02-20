de08e7fdf9e5c1aa88952ec82ab43a879cb20514
Yash <yp349@njit.edu>
2026-02-20 14:44:41 -0500

    changed main branch to daily

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Log the date and memory usage
 
-echo "Memory Log - $(date)" >> /home/yp349/Lab_4/system_log.txt
-free -h | grep Mem >> /home/yp349/Lab_4/system_log.txt
-echo "--------------------------------" >> /home/yp349/Lab_4/system_log.txt
+echo "DAILY MEMORY CHECK - $(date)" >> /home/yash-patel/Lab_4/system_log.txt
+free -h | grep Mem >> /home/yash-patel/Lab_4/system_log.txt
+echo "--------------------------------" >> /home/yash-patel/Lab_4/system_log.txt
