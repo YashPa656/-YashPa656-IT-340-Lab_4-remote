e3a7b5a5db0ff1abe91680e0e905d18df39b70b3
Yash <yp349@njit.edu>
2026-02-20 14:43:01 -0500

    fixed mistake in disk monitor

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Log the date and memory usage
 
-echo "Memory Log - $(date)" >> system_log.txt
+echo "SYSTEM REPORT (Memory) - $(date)" >> /home/yash-patel/Lab_4/system_log.txt
 free -h | grep Mem >> /home/yash-patel/Lab_4/system_log.txt
 echo "--------------------------------" >> /home/yash-patel/Lab_4/system_log.txt
